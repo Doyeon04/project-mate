451e00c22c7494c9212f890772bb7383ba90a819
Doyeon04 <rlaehdus0411@naver.com>
2022-12-26 14:57:52 +0900

    WebMvcConfig 추가

---
+++ src/main/java/com/toy/projectmate/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.toy.projectmate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*");
+    }
+}
